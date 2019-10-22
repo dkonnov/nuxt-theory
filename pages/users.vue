d81cc6103d0060b7f0ea6fd08240775010cb2b9d
Дмитрий Коннов <konnov@outlook.com>
2019-10-22 17:04:35 +0900

    lesson 7-1

--- pages/users.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <section>
-    <h1>Пользователи</h1>
-  </section>
-</template>
---
+++ pages/users/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <h1>Пользователи</h1>
+    <ul>
+      <li v-for="user of users" :key="user">
+        <a href="#" @click.prevent="goto(user)">User: {{ user }}</a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    users: [1, 2, 3, 4, 5]
+  }),
+  methods: {
+    goto(user) {
+      this.$router.push("/users/" + user);
+    }
+  }
+};
+</script>
